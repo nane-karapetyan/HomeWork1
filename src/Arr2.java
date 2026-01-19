c0a8bdda4c5c8a8be13dc34cf47e85995dc92096
Nane Karapetyan <nane.karapetyan.03@gmail.com>
2026-01-19 23:23:49 +0400

    Save homework and other changes in Fun2 branch

--- src/Arr2.java
+++ src/Arr2.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Arr2 {
+    public static void main(String[] args) {
+        int[] arr = {5, -3, 7, -1, 0, -9, 4};
+
+        int index = -1;
+        int value = 0;
+
+        // գնում ենք վերջից սկիզբ
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < 0) {
+                index = i;
+                value = arr[i];
+                break;
+            }
+        }
+
+        if (index == -1) {
+            System.out.println("Բացասական թիվ չի գտնվել");
+        } else {
+            System.out.println("Թիվը = " + value);
+            System.out.println("Ինդեքսը = " + index);
+        }
+    }
 }
--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class SecondLargest {
+    public static void main(String[] args) {
+    int[] arr = {5, 3, 9, 1, 9, 7};
+
+    int max = arr[0];
+    int second = arr[0];
+
+    // գտնում ենք ամենամեծը
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    // գտնում ենք 2-րդ ամենամեծը
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] < max) {
+            second = arr[i];
+            break;
+        }
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] < max && arr[i] > second) {
+            second = arr[i];
+        }
+    }
+
+    System.out.println(second);
+}
 }
