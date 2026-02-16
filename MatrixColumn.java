be1d2e51c86b77a885bf77cced24efc55def412c
Nane Karapetyan <nane.karapetyan.03@gmail.com>
2026-02-16 22:33:30 +0400

    Added MatrixColumn.java

---
+++ MatrixColumn.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MatrixColumn {
+    public static void main(String args[]) {
+        int[][] matrix = {
+                {6, 5, 9, 7, 8},
+                {6, 7, 1, 4, 9},
+                {2, 2, 4, 3, 7}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int firstColumnSum = 0;
+        for (int i = 0; i < rows; i++) {
+            firstColumnSum += matrix[i][0];
+        }
+
+        boolean allEqual = true;
+
+        for (int j = 1; j < cols; j++) {
+            int columnSum = 0;
+
+            for (int i = 0; i < rows; i++) {
+                columnSum += matrix[i][j];
+            }
+
+            if (columnSum != firstColumnSum) {
+                allEqual = false;
+                break;
+            }
+        }
+
+        if (allEqual) {
+            System.out.println("All column sums are equal.");
+        } else {
+            System.out.println("Column sums are NOT equal.");
+        }
+    }
+}
