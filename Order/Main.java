a100e0a6e2d6d58eb096263b482104d7979d1821
Nane Karapetyan <nane.karapetyan.03@gmail.com>
2026-03-05 21:19:38 +0400

    Add Order and Product classes with static members

--- Order/Main.java
+++ Order/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Order.setShippingFee(500.0);
 
-        Order o1 = new Order("Davit", 2000.0); // Default time
-        Order o2 = new Order("Alisa", 3500.0, "10:00 AM"); // Custom time
+        Order o1 = new Order("Davit", 2000.0);
+        Order o2 = new Order("Alisa", 3500.0, "10:00 AM");
 
         System.out.println("Total for John: " + o1.calculateTotal());
         System.out.println("Total for Alice: " + o2.calculateTotal());
