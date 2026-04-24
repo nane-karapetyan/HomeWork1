a51560753b5e1319ff405ccff9034a62e07ce8ee
Nane Karapetyan <nane.karapetyan.03@gmail.com>
2026-04-24 17:52:48 +0400

    feat: implement boarding system with exception priority logic

---
+++ boarding_system/Main.java
@@ -0,0 +1,15 @@
+package boarding_system;
+
+public class Main {
+    public static void main(String[] args) {
+        BoardingService service = new BoardingService();
+        Passenger passenger = new Passenger("Aram", false, false, false);
+
+        try {
+            service.boardPassenger(passenger);
+        } catch (BoardingException e) {
+
+            System.err.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
