9902c328bd9a15e440b5be8699de2b69d40029be
Nane Karapetyan <nane.karapetyan.03@gmail.com>
2026-05-01 17:37:51 +0400

    Remove pharmacy and boarding_system from git, keep only shipping

--- pharmacy/Main.java
+++
@@ -1,9 +0,0 @@
-package pharmacy;
-
-public class Main {
-    public static void main(String[] args) {
-        PillBottle<Aspirin> aspirinBottle = new PillBottle<>();
-        aspirinBottle.fill(new Aspirin());
-
-           }
-}
