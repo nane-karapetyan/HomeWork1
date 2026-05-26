d2a8f09dd6c114c79c9c191e370fd547b5755bdc
Nane Karapetyan <nane.karapetyan.03@gmail.com>
2026-05-26 15:31:58 +0400

    Add NumberSummation class with wildcard methods for Task19

--- task01_2605/Main.java
+++ task01_2605/Main.java
@@ -1,4 +1,26 @@
 package task01_2605;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(10);
+        integerArrayList.add(20);
+        integerArrayList.add(30);
+
+        List<Double> doubleLinkedList = new LinkedList<>();
+        doubleLinkedList.add(5.5);
+        doubleLinkedList.add(10.2);
+        doubleLinkedList.add(4.3);
+
+        double arrayListSum = NumberSummation.sumOfList(integerArrayList);
+        double linkedListSum = NumberSummation.sumOfList(doubleLinkedList);
+
+        System.out.println("ArrayList (Integer) sum " + arrayListSum);
+        System.out.println("LinkedList (Double) sum = " + linkedListSum);
+    }
 }
--- task01_2605/NumberSummation.java
+++ task01_2605/NumberSummation.java
@@ -1,4 +1,17 @@
 package task01_2605;
+import java.util.List;
 
 public class NumberSummation {
+
+      public static double sumOfList(List<? extends Number> list) {
+        double sum = 0.0;
+
+        for (Number number : list) {
+            if (number != null) {
+                sum += number.doubleValue();
+            }
+        }
+
+        return sum;
+    }
 }
